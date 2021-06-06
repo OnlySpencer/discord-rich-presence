5c769beea14998472e40cc679322249756faebf4
OnlySpencer <70861780+OnlySpencer@users.noreply.github.com>
2021-06-06 10:27:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-var rpc = require("discord-rpc")
+var rpc = require('discord-rpc');
+
 const client = new rpc.Client({ transport: 'ipc' })
+
 client.on('ready', () => {
-client.request('SET_ACTIVITY', {
-pid: process.pid,
-activity : {
-details : "Details text",
-assets : {
-large_image : "Image",
-large_text : Text in image"
-},
-buttons : [{label : "Button 1" , url : "link"},{label : "Button 2",url : "link"}]
-}
+    client.sendJoinRequest('SET_ACTIVITY', {
+        pid: process.pid,
+
+        activity : {
+            details : 'Details',
+            assets : {
+                large_image : "name_of_image",
+                large_text : 'text_in_large_image',
+            },
+            buttons : [{
+                label : 'button 1',
+                url : 'link 1'
+            },
+            {
+                label : 'Button 2',
+                url : 'link 2'
+            }]
+        }
+    })
 })
-})
-client.login({ clientId : "Application ID" }).catch(console.error);
+client.login({ clientId : 'Application ID' }).catch(console.error);r);
